ba770928cce369dd3877944f6828138d4ae159d0
Sudha Vijayakumar <69941575+sudha-vijayakumar@users.noreply.github.com>
2022-02-27 23:39:42 -0800

    New RASA actions

--- WIKI_Chatbot/actions/actions.py
+++ WIKI_Chatbot/actions/actions.py
@@ -15,26 +15,21 @@ from rasa_sdk.executor import CollectingDispatcher
 import pyTigerGraph as tg
 
 ################# TigerGraph Credentials ######################""
-
-# Connection parameters
-hostName = "https://language.i.tgcloud.io/"
-userName = "tigergraph"
-password = "tigergraph"
-
-conn = tg.TigerGraphConnection(host=hostName, username=userName, password=password)
-print("Connected")
-
-conn.graphname="WordNet"
-secret = conn.createSecret()
-authToken = conn.getToken(secret)
-authToken = authToken[0]
-conn = tg.TigerGraphConnection(host=hostName, graphname="WordNet", username=userName, password=password, apiToken=authToken)
-
+configs = {
+    "host": "https://language.i.tgcloud.io/",
+    "username": "tigergraph",
+    "password": "tigergraph",
+    "graphname": "WordNet",
+    "secret" : "51gbt2nau9vbjh4m47njh0mmnpnrs6g7"
+    }
 
 ################# TigerGraph Initialization ######################""
-
-conn.gsql("USE graph WordNet")
+conn = tg.TigerGraphConnection(host=configs['host'], username = configs['username'], password=configs['password'], useCert=True, graphname=configs['graphname'])
+conn.apiToken = conn.getToken(configs['secret'])
+conn.gsql("USE graph {}".format(configs['graphname']))
 #######################################"
+
+print("Connected")
 
 class ActionWordDef(Action):
 
@@ -46,24 +41,47 @@ class ActionWordDef(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
 
-        print(tracker.latest_message)
         prediction = tracker.latest_message['entities'][0]['value']
-
+        
         if prediction:
             query_response = conn.runInstalledQuery("get_definition",{"word_to_query": prediction})
             response = query_response[0]
             # print(response)
-            words = response["word_definition"]
-            dispatcher.utter_message("Here is word definition:")
+            words = response["related_definition"]
+            dispatcher.utter_message("Here is the list of definitions:")
             dispatcher.utter_message(words[0])
             dispatcher.utter_message("========================")
 
         return []
 
-class ActionRelatedWords(Action):
+class ActionHypernym(Action):
 
     def name(self) -> Text:
-        return "action_related_words"   # !!!! this return value must match line 55 of domain.yml  [ Step 4.a ]
+        return "action_hypernym"   # !!!! this return value must match line 55 of domain.yml  [ Step 4.a ]
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        print(tracker.latest_message)
+        prediction = tracker.latest_message['entities'][0]['value']
+
+        if prediction:
+            query_response = conn.runInstalledQuery("get_synonyms",{"word_to_query": prediction})
+            response = query_response[0]
+            # print(response)
+            words = response["definition"]
+            dispatcher.utter_message("Here is the list of hypernyms:")
+            dispatcher.utter_message(words[0])
+            dispatcher.utter_message("========================")
+
+        return []
+
+class ActionHyponym(Action):
+
+    def name(self) -> Text:
+        return "action_hyponym"   # !!!! this return value must match line 55 of domain.yml  [ Step 4.a ]
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -77,17 +95,17 @@ class ActionRelatedWords(Action):
             query_response = conn.runInstalledQuery("get_related_words",{"word_to_query": prediction})
             response = query_response[0]
             # print(response)
-            words = response["related_words"]
-            dispatcher.utter_message("Here is the list of related words:")
+            words = response["definition"]
+            dispatcher.utter_message("Here is the list of hyponyms:")
             dispatcher.utter_message(words[0])
             dispatcher.utter_message("========================")
 
         return []
 
-class ActionWordPos(Action):
+class ActionAntonyms(Action):
 
     def name(self) -> Text:
-        return "action_word_pos"   # !!!! this return value must match line 55 of domain.yml  [ Step 4.a ]
+        return "action_antonyms"   # !!!! this return value must match line 55 of domain.yml  [ Step 4.a ]
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -98,11 +116,35 @@ class ActionWordPos(Action):
         prediction = tracker.latest_message['entities'][0]['value']
 
         if prediction:
-            query_response = conn.runInstalledQuery("get_pos",{"word_to_query": prediction})
+            query_response = conn.runInstalledQuery("get_antonyms",{"word_to_query": prediction})
             response = query_response[0]
             # print(response)
-            words = response["related_pos"]
-            dispatcher.utter_message("Here is the list of POS:")
+            words = response["definition"]
+            dispatcher.utter_message("Here is the list of antonyms:")
+            dispatcher.utter_message(words[0])
+            dispatcher.utter_message("========================")
+
+        return []
+
+class ActionAllConnections(Action):
+
+    def name(self) -> Text:
+        return "action_all_connections"   # !!!! this return value must match line 55 of domain.yml  [ Step 4.a ]
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        print(tracker.latest_message)
+        prediction = tracker.latest_message['entities'][0]['value']
+
+        if prediction:
+            query_response = conn.runInstalledQuery("get_all_connections",{"word_to_query": prediction})
+            response = query_response[0]
+            # print(response)
+            words = response["definition"]
+            dispatcher.utter_message("Here is the list of related words:")
             dispatcher.utter_message(words[0])
             dispatcher.utter_message("========================")
 
